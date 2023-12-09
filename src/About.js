d5343b6b53b16b4fb66cd3cfc0c570a0564a8fa7
Praveen-Kr-S <praveenkumar328be@gmail.com>
2023-12-09 14:35:25 +0530

    Create File

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    
+    <main className='About'>
+        <h2>About</h2>
+        <p style={{marginTop: "1rem"}}>Hi, I am Praveen Kumar . Together lets make education free for everyone in the world</p>
+    </main>
+  )
+}
+
+export default About
---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./About";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import Missing from "./Missing";
+import Nav from "./Nav";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import EditPost from "./EditPost";
+
+import { DataProvider } from "./context/DataContext";
+ 
+function App() {
+
+  return (
+    <div className="App">
+      
+      <DataProvider>
+
+        <Header title="Social Post Media" />
+
+        <Nav />
+
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+
+          <Route path="post">
+
+            <Route index element ={<NewPost />} />
+
+            <Route path=":id" element ={<PostPage />} />
+            
+          </Route>
+
+          <Route path="/edit/:id" element={<EditPost />} />
+
+          <Route path="About" element = {<About />} />
+
+          <Route path="*" element={<Missing />} />
+
+        </Routes>
+        
+        <Footer />
+
+      </DataProvider> 
+
+    </div>
+  );
+}
+
+export default App;
